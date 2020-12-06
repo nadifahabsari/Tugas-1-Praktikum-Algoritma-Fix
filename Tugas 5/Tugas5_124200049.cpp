e4fa09a41dce6fb908f03f716cc66c8d5f5e8a24
Nadifa Habsari <nadifahabsari@gmail.com>
2020-12-06 23:12:46 +0700

    tugas 5

---
+++ Tugas 5/Tugas5_124200049.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int main()
+{ int panjang, lebar, pilih;
+  int tinggi;
+		cout <<"Gambar Bangun Datar ==========="<<endl;
+	    cout <<"1. Kotak Bolong"<<endl;
+	    cout <<"2. Segitiga"<<endl;
+	    cout <<"Pilih : "; cin>>pilih;
+	    
+	    switch (pilih){
+			case 1:
+			cout <<endl;
+			cout <<"Panjang : "; cin>>panjang;
+			cout <<"Lebar   : "; cin>>lebar;
+			for (int i=1; i<=panjang; i++) {
+				for (int j=1; j<=lebar; j++) {
+					if (i==1 || i==panjang || j==1 || j==lebar) {
+					cout <<"* ";
+				} else {
+					cout <<"  ";
+				}
+				}
+					cout <<endl;
+				}
+			break;
+			case 2:
+			cout <<endl;
+			cout << "Tinggi : "; cin>>tinggi;
+			for (int a=1; a<=tinggi; a++) {
+				int x = 1;
+				int y = a;
+				cout <<"";
+				for (int b=1; b<=a; b++) {
+					cout << y <<" ";
+					y = y + (tinggi-x);
+					x++;
+				}
+				cout <<"\n";
+				}
+				break;
+				}
+		return 0;
+}
